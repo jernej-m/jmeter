fb856bb086723e50aee06a655f1f6ce0f097185a
PostgreSQL Server <postgres@ip-172-31-21-6.eu-central-1.compute.internal>
2020-01-06 13:09:23 +0000

    added create_table.sql

---
+++ create_table.sql
@@ -0,0 +1,13 @@
+create table test
+(
+    col_1 text,
+    col_2 text,
+    col_3 text,
+    col_4 text,
+    col_5 text,
+    col_6 text,
+    col_7 text,
+    col_8 text,
+    col_9 text,
+    col_10 text
+);
